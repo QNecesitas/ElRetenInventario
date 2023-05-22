290ec454b07d99acec3331db184fae4294ef5679
Marcos Suarez <markozsuarez01@gmail.com>
2023-05-22 17:09:54 +0200

    Problems are fixed

--- app/src/main/java/com/qnecesitas/elreteninventario/auxiliary/Constants.kt
+++ app/src/main/java/com/qnecesitas/elreteninventario/auxiliary/Constants.kt
@@ -9,7 +9,7 @@ class Constants {
         const val PHP_IMAGES = "https://qnecesitas.nat.cu/ElReten/Images/"
 
         //Tokens
-        const val PHP_TOKEN = "ElReten_820"
+        const val PHP_TOKEN = "ElReten_380"
 
 
     }
