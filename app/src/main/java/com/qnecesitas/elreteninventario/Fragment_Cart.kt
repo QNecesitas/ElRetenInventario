d154178fad3e4d574d315da272333da6f4a5dc4d
Ronny Nordelo <ronnynordelo@gmail.com>
2023-06-04 16:31:20 -0400

    Fixing problems to starting migrating to local DB

--- app/src/main/java/com/qnecesitas/elreteninventario/Fragment_Cart.kt
+++ app/src/main/java/com/qnecesitas/elreteninventario/Fragment_Cart.kt
@@ -3,7 +3,6 @@ package com.qnecesitas.elreteninventario
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +11,6 @@ import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import com.qnecesitas.elreteninventario.adapters.AdapterR_CounterProductAdd
 import com.qnecesitas.elreteninventario.auxiliary.Constants
 import com.qnecesitas.elreteninventario.auxiliary.NetworkTools
 import com.qnecesitas.elreteninventario.data.ModelCart
