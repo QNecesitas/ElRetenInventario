d53c0151c7931ed41c9d8a97311f5bdd8327b341
Ronny Nordelo <ronnynordelo@gmail.com>
2023-06-04 17:05:30 -0400

    Problem merging was fixed

--- app/src/main/java/com/qnecesitas/elreteninventario/Fragment_Cart.kt
+++ app/src/main/java/com/qnecesitas/elreteninventario/Fragment_Cart.kt
@@ -11,6 +11,7 @@ import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.qnecesitas.elreteninventario.adapters.AdapterR_CounterProductAdd
 import com.qnecesitas.elreteninventario.auxiliary.Constants
 import com.qnecesitas.elreteninventario.auxiliary.NetworkTools
 import com.qnecesitas.elreteninventario.data.ModelCart
